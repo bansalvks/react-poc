f7e7a2caeecf89739232c62e30d20e1a5f5a92c5
B0208143 <b0208143@LTB0208143-MAC.local>
2018-10-24 15:56:56 +0530

    poc on scss with react

---
+++ scss-with-react/README.md
@@ -0,0 +1,8 @@
+## POC on sass integration and watch with node-sass-chokidar
+https://github.com/michaelwayman/node-sass-chokidar
+
+## Start
+```npm start```
+
+## Build
+```npm build```
---
+++ scss-with-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <span className="welcome">Hello World</span>
+      </div>
+    );
+  }
+}
+
+export default App;
